4bc38d5ecc43c8fe0362b0cff802dda2b5f6ebeb
D:\Zip\Bat_Mail <devnull@localhost>
2013-06-24 16:32:12 +0400

    ZD-2065 ???????? v1 ??????? ????

--- my_uu/settings.py
+++ my_uu/settings.py
@@ -29,8 +29,8 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'pvoytko',
         'PASSWORD': 'zQB5NDos',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': '127.0.0.1',       # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                # Set to empty string for default.
     }
 }
 
