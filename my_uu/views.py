962241736df4726e54e74bb8b2b3106057ffdb53
D:\Zip\Bat_Mail <devnull@localhost>
2013-06-24 15:25:46 +0400

    ZD-2065 ???????? v1 ??????? ????

--- my_uu/views.py
+++ my_uu/views.py
@@ -12,17 +12,6 @@ import json
 import django.db.utils
 
 
-# Это нужно чтобы подружить angular и django по теме csrf-protection. Взяо отсюда:
-# https://github.com/whitehat2k9/kevinzhang.org/blob/master/posts/django-angularjs-and-csrf-xsrf-protection.md
-class AngularCSRFRename(object):
-    ANGULAR_HEADER_NAME = 'HTTP_X_XSRF_TOKEN'
-    def process_request(self, request):
-        if self.ANGULAR_HEADER_NAME in request.META:
-            request.META['HTTP_X_CSRFTOKEN'] = request.META[self.ANGULAR_HEADER_NAME]
-            del request.META[self.ANGULAR_HEADER_NAME]
-        return None
-
-
 # Главная страница.
 # Вызывает шаблон главной страницы.
 def main(request):
