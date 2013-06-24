3010442953bc79f9d6bb9ceb718b58fb545b778b
D:\Zip\Bat_Mail <devnull@localhost>
2013-06-24 15:37:37 +0400

    ZD-2065 ???????? v1 ??????? ????

--- my_uu/settings.py
+++ my_uu/settings.py
@@ -24,11 +24,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'my_uu',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'USER': 'pvoytko',
+        'PASSWORD': 'zQB5NDos',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
