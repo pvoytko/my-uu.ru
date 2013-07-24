b421d7b180ae7818383a5011ca00facfe8cec083
D:\Zip\Bat_Mail <devnull@localhost>
2013-07-25 00:26:48 +0400

    ZD-2209 ???????? ???????? ?? ???????? ????? ?????? ?? ??? ????????? ?? ????? ??????? ?? ???????

--- my_uu/cron_request_feedback.py
+++ my_uu/cron_request_feedback.py
@@ -6,6 +6,9 @@
 
 # Подключение Джанги
 import os, sys
+virtual_env = os.path.expanduser('~/virtualenv/MyEnv')
+activate_this = os.path.join(virtual_env, 'bin/activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
 sys.path.insert(0, os.path.join(os.path.expanduser('~'), 'domains/my-uu.ru'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'my_uu.settings'
 
