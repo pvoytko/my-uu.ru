9c6ecac63a61030a57054e66093263c2347955f8
Voytko <pvoytko@gmail.com>
2020-05-18 10:47:32 +0200

    PL-2813 pva

--- my_uu/static/js/angular_project_1.js
+++ my_uu/static/js/angular_project_1.js
@@ -70,14 +70,19 @@ angular.module('myNgApp').controller('FinCalcCtrl', function($scope, $http){
     // Функция заполенния расчета
     $scope.fccUpdateRaschet = function(){
 
+        // Когда достигнута цель, пустое - значит не достигнута
+        $scope.msi_goal1_reached_str = "";
+        $scope.msi_goal2_reached_str = "";
+
         // Заполняет месяцы
         $scope.fcc_months_array = [];
         for (var m_num=1; m_num<=$scope.msi_srok_let_val*12; ++m_num){
 
-            // Очередной месяц
+            // Очередной месяц и год
             var cmo = {};
             cmo.fcc_month_total_num = m_num;
             cmo.fcc_month_year_num = ((m_num-1) % 12)+1;
+            cmo.fcc_year_num = parseInt((m_num - 1) / 12);
 
             // Надо ли годовой заголовок
             cmo.fcc_year_header_num = $scope.getYearForMonth(m_num);
@@ -95,9 +100,27 @@ angular.module('myNgApp').controller('FinCalcCtrl', function($scope, $http){
             cmo.fcc_doinvest_val = (m_num <= $scope.msi_add_let_val*12) ? $scope.msi_add_rub_val : 0;
             cmo.fcc_doinvest_str = pvlNumberWithRoubles(cmo.fcc_doinvest_val);
 
+            // А затем лишь доля, а остальное изымается. Так же присваиваем ноль чтобы не было
+            // пустых клеток первое время.
+            cmo.fcc_dodod_izym_val = 0;
+            cmo.fcc_dodod_izym_str = pvlNumberWithRoubles(cmo.fcc_dodod_izym_val);
+            if (m_num > $scope.msi_reinvest_let_val*12){
+
+                cmo.fcc_dodod_izym_val = parseInt($scope.msi_vivod_proc_val / 100 * cmo.fcc_month_proc_val);
+                cmo.fcc_dodod_izym_str = pvlNumberWithRoubles(cmo.fcc_dodod_izym_val);
+            }
+
+            // Как только доход превысил цель, этот месяц запоминаем
+            if (!$scope.msi_goal1_reached_str && (cmo.fcc_month_proc_val > $scope.msi_goal1_rub_val)){
+                $scope.msi_goal1_reached_str = cmo.fcc_year_num + "-й год, " + cmo.fcc_month_year_num + "-й месяц" ;
+            }
+            if (!$scope.msi_goal2_reached_str && (cmo.fcc_month_proc_val > $scope.msi_goal2_rub_val)){
+                $scope.msi_goal2_reached_str = cmo.fcc_year_num + "-й год, " + cmo.fcc_month_year_num + "-й месяц" ;
+            }
+
 
             // На конец месяца считается как значение на началом + проценты + доинвестирование
-            cmo.fcc_full_nakop_end_month_val = cmo.fcc_full_nakop_begin_month_val + cmo.fcc_month_proc_val + cmo.fcc_doinvest_val;
+            cmo.fcc_full_nakop_end_month_val = cmo.fcc_full_nakop_begin_month_val + cmo.fcc_month_proc_val - cmo.fcc_dodod_izym_val + cmo.fcc_doinvest_val;
             cmo.fcc_full_nakop_end_month_str = pvlNumberWithRoubles(cmo.fcc_full_nakop_end_month_val);
 
             $scope.fcc_months_array.push(cmo);
@@ -116,6 +139,8 @@ angular.module('myNgApp').controller('FinCalcCtrl', function($scope, $http){
     $scope.$watch('msi_reinvest_let_val', $scope.fccUpdateRaschet);
     $scope.$watch('msi_vivod_proc_val', $scope.fccUpdateRaschet);
     $scope.$watch('msi_srok_let_val', $scope.fccUpdateRaschet);
+    $scope.$watch('msi_goal1_rub_val', $scope.fccUpdateRaschet);
+    $scope.$watch('msi_goal2_rub_val', $scope.fccUpdateRaschet);
     $scope.fccUpdateRaschet();
 
 });
